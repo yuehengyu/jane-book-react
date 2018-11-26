00100b19cc2f69d6be774ff0034f2f03325b53f4
yuehengyu <44220326+yuehengyu@users.noreply.github.com>
2018-11-26 01:38:03 -0800

    aa

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import logoPic from '../../statics/logo.png';
+import logoPic from '../../statics/logo.jpg';
 
 export const HeaderWrapper=styled.div`
     z-index:1;
